dc2b59f0db0ac44e7e0f639a432c85f4d806bf07
Aditya Thakral <aditya.1998thakral@gmail.com>
2017-07-12 01:24:52 -0400

    Add LED class and finalized Visualizer
    The Visualizer Array is now an Array of an array of LED objects.
    LED class has mothods like set color, led off, etc

--- Visualizer.py
+++ Visualizer.py
@@ -2,6 +2,33 @@ import neopixel
 import time
 
 
+class LED(object):
+    def __init__(self, index, color):
+        self.index = index
+        self.color = color
+
+    def get(self, prop='led'):
+        if not self.index: return False
+        if prop == 'index':
+            return self.index
+        elif prop == 'color':
+            return self.color
+        elif prop == 'led':
+            return [self.index, self.color]
+
+    def setColor(self, color):
+        self.color = color
+
+    def colorLED(self, strip, show=False):
+        strip.setPixelColor(self.index, neopixel.Color(*(self.color)))
+        if show: strip.show()
+
+    def off(self, strip=None, show=False):
+        self.setColor([0, 0, 0])
+        if strip != None:
+            self.colorLED(strip, show)
+
+
 class Visualizer(object):
     def __init__(self, numberOfLeds, numberOfBars, x_direction, y_direction):
         self.numberOfLeds = numberOfLeds
@@ -22,6 +49,7 @@ class Visualizer(object):
             self.channel,
             self.stripType)
         self.strip.begin()
+        self.show = self.strip.show
         self.x_flow = x_direction
         self.y_flow = y_direction
         self.visualizerArray = []
@@ -37,24 +65,51 @@ class Visualizer(object):
                 index = (x * 15) + y \
                     if x % 2 == vertical_bool \
                     else (x + 1) * 15 - (y + 1)
-                led = [index, color]
+                led = LED(index, color)
                 bar.append(led)
             self.visualizerArray.append(bar)
         if self.x_flow != 'right':
             self.visualizerArray.reverse()
 
-    def color_led(self, led, show=True):
-        self.strip.setPixelColor(led[0], neopixel.Color(*(led[1])))
-        if show: self.strip.show()
+    def color_bar(self, bar_position):
+        for i in range(self.numberOfLedsPerBar):
+            self.visualizerArray[bar_position][i].colorLED(self.strip)
+        self.show()
 
-    def color_bar(self, bar):
-        for led in bar:
-            self.color_led(led)
-            time.sleep(0.1)
+    def color_array(self):
+        for bar in self.visualizerArray:
+            for led in bar:
+                led.colorLED(self.strip)
+        self.show()
+
+    def set_bar_percent(self, bar_position, percent, color=[255, 255, 255]):
+        if not (0 <= bar_position < self.numberOfBars): return False
+
+        number_of_leds = int(round(percent * self.numberOfLedsPerBar))
+        for i in range(number_of_leds):
+            self.visualizerArray[bar_position][i].setColor(color)
+        for i in range(number_of_leds, self.numberOfLedsPerBar):
+            self.visualizerArray[bar_position][i].off()
 
     def wipe(self, color=[0, 0, 0]):
-        for x in range(self.numberOfBars):
-            for y in range(self.numberOfLedsPerBar):
-                self.visualizerArray[x][y][1] = color
         for bar in self.visualizerArray:
-            self.color_bar(bar)
+            for led in bar:
+                led.setColor(color)
+                led.colorLED(self.strip)
+            self.show()
+
+
+from random import random
+import time
+
+v = Visualizer(150, 10, 'left', 'down')
+v.wipe()
+for i in range(1000):
+    for i in range(10):
+        red = int(round(random() * 255))
+        green = int(round(random() * 255))
+        blue = int(round(random() * 255))
+        color = [red, green, blue]
+        v.set_bar_percent(i, random(), color)
+    v.color_array()
+    time.sleep(0.04)
